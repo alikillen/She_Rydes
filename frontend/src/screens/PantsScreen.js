8a7eae3ccf6b97ce4fab7aa5a28f0e82934491c9
alikillen <alisoneme1994@gmail.com>
2021-01-09 19:16:11 +1000

    all product pages working!

--- frontend/src/screens/PantsScreen.js
+++ frontend/src/screens/PantsScreen.js
@@ -1,44 +1,56 @@
-import React, { Component, useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux"
-import {Row, Col} from "react-bootstrap"
+import React, { useEffect } from 'react'
 import Product from "../components/Product"
-import Message from "../components/Message"
-import Loader from "../components/Loader"
-import { listProducts, listProductsByCategory } from "../actions/productActions.js"
+import { Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductsByCategory } from '../actions/productActions'
 
 const PantsScreen = () => {
+  
   const dispatch = useDispatch()
 
-  const productList = useSelector(state => state.productList)
-  const { loading, error, products } = productList
-  // these are parts of the state that could be sent down
-
-  useEffect(() => {
-    dispatch(listProductsByCategory())
-  }, [dispatch])
-  // this calls listProducts and fills up our state by passing the payloads into the reducer
+  const productCategory = useSelector((state) => state.productCategory)
+  const { loading, error, products } = productCategory
 
 
+  try {
 
-  return (
-    <>
-      <h1>Pants</h1>
-      {loading ? (
-        <Loader /> )
-        : error ? (
-          <Message variant="danger">{error}</Message>
-        ) : (
-      <Row>
-          {products.map(product=>
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          )}
-        </Row>) 
-      }
-        
-    </>
-  )
+    useEffect(() => {
+      dispatch(listProductsByCategory("Pants"))
+    }, [dispatch])
+
+    console.log("useeffect dispatched, should return")
+  
+
+    return (
+      <div>
+  
+        <h1>Pants</h1>
+        {loading ? (
+          <Loader /> )
+          : error ? (
+            <Message variant="danger">{error}</Message>
+          ) : (
+        <Row>
+            {products.map(product=>
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            )}
+          </Row>) 
+        }
+          
+      </div>
+    )
+
+    // no errors but nothing returned
+    
+  } catch (error) {
+    console.log("cant return page")
+  }
+
+  
 }
 
 export default PantsScreen
