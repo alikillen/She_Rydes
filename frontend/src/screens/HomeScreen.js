43f7d6b9683a4a279cf4d86f027e00cb6860e24d
Sevda Amini-Uhde <sevda.amini@gmail.com>
2021-01-12 15:02:52 +1000

    homepage almost done

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,235 +1,202 @@
 import React, { Component, useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux"
-import {Row, Col, Container, Form} from "react-bootstrap"
+import {Row, Col, Container, Form, Button, Image} from "react-bootstrap"
 import Product from "../components/Product"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
 import { listProducts } from "../actions/productActions.js"
 import Carousel from 'react-bootstrap/Carousel'
+import backgroundVideo from '../video/backgroundVideo.mp4'
+
+
 
 
 
 const HomeScreen = () => {
-  // const dispatch = useDispatch()
-
-  // const productList = useSelector(state => state.productList)
-  // const { loading, error, products } = productList
-  // // these are parts of the state that could be sent down
-
-  // useEffect(() => {
-  //   dispatch(listProducts())
-  // }, [dispatch])
-  // // this calls listProducts and fills up our state by passing the payloads into the reducer
-
-
 
   return (
     <>
-    {/* <div className="Container">
-    <div>
-    <img style={{ 
-      backgroundImage: "url(/images/HomeScreenImg/bg.png)",
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      width:'100vw',
-      height: '100vh'
-       }}/>
-    </div>
- 
-       <div>
-       <h3>
-        Along the coastline, 
-          <br></br>
-        into the outback, 
-          <br></br>
-        among the mountains 
-          <br></br>
-        She Rydes . . .
-    </h3>
-       </div>
+      <div className="video-section">
+        <video autoPlay loop muted  id='video'>
+          <source src={ backgroundVideo } type='video/mp4'/>
+        </video>
+
+        <div className="video-text">
+        <p>
+        <span id="sherydes-bold">She Rydes</span> is an Australian owned, Gold Coast based 
+        <br />
+        company that is dedicated to providing women
+        riders <br />
+        with quality protective gear. <br />
+        Our vision is to empower your ride with gear that is
+        <br />
+        affordable and effortlessly stylish
+        </p>
+        </div>
+
+        <div className="explore-range-text">
+          <p className="animate__animated animate__fadeInLeft">Explore the range now...</p>
+          <div>
+            <Button size="lg" id="explore-range-button" className="animate__animated animate__fadeInLeft">
+              Check our Products
+            </Button>{' '}
+          </div>
+        </div>
      
-    </div> */}
-    
-
-  
-
-   
-    
-
-
-
-    {/* style={{ 
-      backgroundImage: "url(/images/Jackets/bg.png)",
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      width:'100vw',
-      height: '100vh'
-       }} */}
-
-  
- <Carousel className='custom-carousel'>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/images/HomeScreenImg/jacket-carousel.jpg"
-        alt="First slide"
-      />
-    
-    <Carousel.Caption className='custom-carousel-caption'>
         
-    <h3>
-        Along the coastline, 
-          <br></br>
-        into the outback, 
-          <br></br>
-        among the mountains 
-          <br></br>
-        She Rydes . . .
-    </h3>
-       </Carousel.Caption>
-    </Carousel.Item>
+      </div>
+  
 
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/images/HomeScreenImg/gloves-carousel.jpg"
-        alt="Third slide"
-      />
-      <Carousel.Caption className='custom-carousel-caption'>
-      <h3>
-        Along the coastline, 
-          <br></br>
-        into the outback, 
-          <br></br>
-        among the mountains 
-          <br></br>
-        She Rydes . . .
-      </h3>
-      </Carousel.Caption>
-
-    </Carousel.Item>
-    <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src="/images/HomeScreenImg/jeans-carousel.jpg"
-        alt="Third slide"
-      />
-
-      <Carousel.Caption className='custom-carousel-caption'>
-      <h3>
-        Along the coastline, 
-          <br></br>
-        into the outback, 
-          <br></br>
-        among the mountains 
-          <br></br>
-        She Rydes . . .
-      </h3>
-      </Carousel.Caption>
-    </Carousel.Item>
-  </Carousel>
+{/* Static picture and words animation */}
+  
+    <div style={{
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/images/Jackets/static-bg.png'})`,
+      backgroundRepeat: 'no-repeat',
+      width: '100vw',
+      height: '100vh',
+      
+    }}>
+    
+    <div className="poem">
+       {/* <p id="poem-paragraph"> */}
+       <div data-aos="fade-up" data-aos-delay="500" data-aos-duration="2000" className="line-one">Along the coastline</div>
+       <div data-aos="fade-up" data-aos-delay="600"  data-aos-duration="2000" className="line-two">into the outback</div>
+       <div data-aos="fade-up" data-aos-delay="700" data-aos-duration="2000"  className="line-three">among the mountains</div>
+       <div data-aos="fade-up" data-aos-delay="800" data-aos-duration="2000" className="line-four" id='sherydes-bold'>She Rydes . . .</div>
+    </div>
+   
+      
+       
+        
+        
+    </div>
+     
   
  {/* SHOP THE LOOK SECTION */}
  
 <div className='shop-the-look'>
-  <h3>Shop The Look</h3>
-  <Container>
-  <Row>
+<div id="shop-the-look-div">
+<h3 id='shop-the-look-header'>Shop The Look</h3>
+</div>
+
+  
+<Container>
+  <Row className="carousel-custom">
     <Col sm>
-    <a href='#'>
-    <img
-        // add link to the related gear
-        className="custom-style-img"
-        src="/images/HomeScreenImg/Black-denim-front-lifestyle.png"
-        alt="shop the look image"
-      />
-    </a>
+      <Carousel data-aos="fade-down" data-aos-delay="500" data-aos-duration="2000">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+      </Carousel>
     </Col>
     <Col>
-    <a href='#'>
-    <img
-        // add link to the related gear
-        className="custom-style-img"
-        src="/images/HomeScreenImg/Black-glove-lifestyle.png"
-        alt="Shop the look second image"
-      />
-    </a>
+      <Carousel data-aos="fade-down" data-aos-delay="1500" data-aos-duration="2000">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/images/Jackets/blue-bird-front.jpg"
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+          <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        </Carousel>
     </Col>
  
-  
-
     <Col sm>
-    <a href='#'>
-    <img
-        // add link to the related gear
-        className="custom-style-img"
-        src="/images/HomeScreenImg/Pink-sunset-back-lifestyle.png"
-        alt="Shop the look third image"
-      />
-    </a>
-    
-        
+      <Carousel data-aos="fade-down" data-aos-delay="2000" data-aos-duration="2000">
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="/images/Jackets/blue-bird-front.jpg"
+              alt="First slide"
+            />
+            {/* <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+          <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/images/Jackets/blue-bird-front.jpg"
+            alt="First slide"
+          />
+          {/* <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption> */}
+        </Carousel.Item>
+        </Carousel>
     </Col>
 
 
   </Row>
-
-  {/* NEW ARRIVALS */}
-
-
-  <div><h3>NEW Arrivals</h3></div>
-  <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/images/Jackets/blue-bird-front.jpg"
-      alt="First slide"
-    />
-    {/* <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/images/Gloves/black-gloves-3.jpg"
-      alt="Third slide"
-    />
-
-    {/* <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/images/Jackets/outback-outfitter-tan-front.jpg"
-      alt="Third slide"
-    />
-
-    {/* <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="/images/Accessories/coastal-cruiser-patch.jpg"
-      alt="Third slide"
-    />
-
-    {/* <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption> */}
-  </Carousel.Item>
-</Carousel>
-
-
 
 </Container>
 </div>  
