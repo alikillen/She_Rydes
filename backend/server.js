5b770a8fc958a9edab060cd39d63bd627836327b
alikillen <alisoneme1994@gmail.com>
2021-01-17 14:31:06 +1000

    changed port

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,10 @@ app.use(notFound)
 app.use(errorHandler)
 
  
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+// changed for heroku
+
+app.listen(process.env.PORT || 5000)
+// (PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
